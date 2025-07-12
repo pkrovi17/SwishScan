c800e3a64f165b50508e9612be2b042c6fd14832
Noah Ham <noahham25@gmail.com>
2025-07-12 13:43:01 -0700

    starting app code

---
+++ ballin/ballin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ballin
+//
+//  Created by Noah Ham on 7/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ballin/ballin/ballinApp.swift
@@ -0,0 +1,17 @@
+//
+//  ballinApp.swift
+//  ballin
+//
+//  Created by Noah Ham on 7/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ballinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
