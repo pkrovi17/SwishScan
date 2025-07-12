251a309332d4a88f96c4d8d93b8dac7323ad71ac
Noah Ham <noahham25@gmail.com>
2025-07-12 15:25:23 -0700

    quite possibly the most beautiful app ever created

--- ballin/ballin/ContentView.swift
+++ ballin/ballin/ContentView.swift
@@ -10,12 +10,61 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                Button(action: {
+                      // action to perform when the button is tapped
+                    }) {
+                        Image(systemName: "gear")
+                        .resizable() // This allows the image to be resized
+                        .frame(width: 30, height: 30) // This sets the size of the image
+                    }
+                Spacer()
+                Text("Hello, Person")
+                    .font(.title3) // Makes it bigger
+                    .bold()       // Makes it bold
+                Spacer()
+            }
+            Spacer()
+            HStack {
+                Button("Accuracy") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }
+                .padding()
+                .background(Color(red: 0, green: 0, blue: 0.5))
+                .clipShape(RoundedRectangle(cornerRadius:15))
+
+                Button("Form") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }
+                .padding()
+                .background(Color(red: 0, green: 0, blue: 0.5))
+                .clipShape(RoundedRectangle(cornerRadius:15))
+            }
+            Spacer()
         }
-        .padding()
+    }
+}
+
+struct CameraView: View {
+    var body: some View {
+        ZStack {
+            Color.black
+                .ignoresSafeArea(edges: .all)
+            VStack {
+                
+                VStack {
+                    Text("Put the instructions here.")
+                        .padding(.horizontal, 20)
+                        .colorInvert()
+                    Spacer()
+                    Button(action: {}, label: {
+                            Circle()
+                                .fill(Color.white)
+                                .frame(width: 70, height: 70)
+                    })
+                }
+            }
+        }
     }
 }
 
