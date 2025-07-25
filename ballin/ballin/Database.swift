5611c805c3fab45895d77b0b13ae6ea48974a517
Noah Ham <noahham25@gmail.com>
2025-07-24 20:56:54 -0700

    added YOU tab and added top rectangle

--- ballin/ballin/Database.swift
+++ ballin/ballin/Database.swift
@@ -5,6 +5,13 @@ struct GridSquare: View {
     let id = UUID()
     let imageName: String
     let title: String
+    let isUser: Bool
+    
+    init(imageName: String, title: String, isUser: Bool = false) {
+        self.imageName = imageName
+        self.title = title
+        self.isUser = isUser
+    }
     
     var body: some View {
         Button(action: {
@@ -14,17 +21,17 @@ struct GridSquare: View {
                 Image(systemName: imageName)
                     .resizable()
                     .scaledToFit()
-                    .foregroundColor(Color("secondaryButtonText"))
+                    .foregroundColor(self.isUser ? .accentColor : Color("secondaryButtonText"))
                     .frame(height: 60)
                     .padding(.top)
                 
                 Text(title)
                     .font(.headline)
-                    .foregroundColor(Color("secondaryButtonText"))
+                    .foregroundColor(self.isUser ? .accentColor : Color("secondaryButtonText"))
                     .padding(.top, 8)
             }
             .frame(width: 160, height: 200)
-            .background(Color("secondaryButtonBackground"))
+            .background(self.isUser ? Color("buttonBackground") : Color("secondaryButtonBackground"))
             .cornerRadius(15)
         }
         
@@ -37,26 +44,32 @@ struct DatabaseView: View {
     let columns: [GridItem] = Array(repeating: .init(.flexible(), spacing: -12), count: 2)
 
     var body: some View {
-        ScrollView {
-            LazyVGrid(columns: columns, spacing: 16) {
-                GridSquare(imageName: "person.fill", title: "le bon bon")
-                    .padding(.top, 32)
-                GridSquare(imageName: "person.fill", title: "Name")
-                    .padding(.top, 32)
-                GridSquare(imageName: "person.fill", title: "Name")
-                GridSquare(imageName: "person.fill", title: "Name")
-                GridSquare(imageName: "person.fill", title: "Name")
-                GridSquare(imageName: "person.fill", title: "Name")
-                GridSquare(imageName: "person.fill", title: "Name")
-                GridSquare(imageName: "person.fill", title: "Name")
-                GridSquare(imageName: "person.fill", title: "Name")
-                    .padding(.bottom, 32)
-                GridSquare(imageName: "person.fill", title: "Name")
-                    .padding(.bottom, 32)
+        ZStack (alignment: .top){
+            ScrollView {
+                LazyVGrid(columns: columns, spacing: 16) {
+                    GridSquare(imageName: "person.fill", title: "You", isUser: true)
+                        .padding(.top, 32)
+                    GridSquare(imageName: "person.fill", title: "Name")
+                        .padding(.top, 32)
+                    GridSquare(imageName: "person.fill", title: "Name")
+                    GridSquare(imageName: "person.fill", title: "Name")
+                    GridSquare(imageName: "person.fill", title: "Name")
+                    GridSquare(imageName: "person.fill", title: "Name")
+                    GridSquare(imageName: "person.fill", title: "Name")
+                    GridSquare(imageName: "person.fill", title: "Name")
+                    GridSquare(imageName: "person.fill", title: "Name")
+                        .padding(.bottom, 32)
+                    GridSquare(imageName: "person.fill", title: "Name")
+                        .padding(.bottom, 32)
+                }
+                .padding()
             }
-            .padding()
+            .padding(.bottom, 8)
+            Rectangle()
+                .fill(Color(.systemBackground))
+                .frame(maxWidth: .infinity, minHeight: 60, maxHeight: 60)
+                .ignoresSafeArea(edges: .top)
         }
-        .padding(.bottom, 8)
     }
 }
 
