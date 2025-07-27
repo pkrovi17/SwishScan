50402c9f1b6e8c8af137ccc9f797f2461a66324e
Noah Ham <noahham25@gmail.com>
2025-07-27 11:20:43 -0700

    added printPlayerIDs

--- makeDataFiles.py
+++ makeDataFiles.py
@@ -1,79 +1,3 @@
-import json
-from nba_api.stats.endpoints import ShotChartDetail
-from nba_api.stats.static import players
-
-def getPlayerNamesFromFile(file_path: str) -> list[str]:
-    """
-    Read player names from a file and return them as a list.
-
-    Args:
-        file_path (str): The path to the file containing player names.
-
-    Returns:
-        list[str]: A list of player names.
-
-    """
-
-    with open(file_path, 'r') as file:
-        player_names = [line.strip() for line in file if line.strip()]
-    return player_names
-
-def getPlayerID(name: str) -> int or None:
-    """
-    Get the player ID for a given player name.
-
-    Args:
-        name (str): The name of the player.
-
-    Returns:
-        int: The player ID.
-    """
-    matches = players.find_players_by_full_name(name)
-    if matches:
-        return matches[0]['id']
-    return None
-
-def getSeasonShots(player_id: int, season: str = '2024-25') -> list[dict]:
-    """
-    Gets the shot data for a specific player in a given season.
-
-    Args:
-        player_id (int): The ID of the player.
-        season (str): The season for which to retrieve shot data (default is '2024-25').
-
-    Returns:
-        list[dict]: A list of dictionaries containing shot data for the player.
-    """
-    shotlog = ShotChartDetail(
-        team_id = 0,  # 0 = all teams
-        player_id = player_id,
-        season_nullable = '2024-25',
-        season_type_all_star = 'Regular Season',
-        context_measure_simple = 'FGA'  # important to include missed shots
-    )
-
-    return shotlog.get_data_frames()[0]
-
-if __name__ == "__main__":
-    # For each player in the file, get their shot data and save it as a JSON file
-    for name in getPlayerNamesFromFile('data/players.txt'):
-        player_id = getPlayerID(name)
-        if player_id:
-            shots = getSeasonShots(player_id, '2024-25')[["SHOT_MADE_FLAG", "LOC_X", "LOC_Y"]]
-
-            # Filter to only less than half court, then reorient to the center of half-court
-            shots = shots[shots['LOC_Y'].between(0, 564)].copy()
-            shots['LOC_Y'] = shots['LOC_Y'] - 282
-
-            json_data = {
-                "name": name,
-                "shots": shots.to_dict(orient='records')
-            }
-
-            # Saves as player_id.json to prevent accent file naming issues
-            with open(f"data/{player_id}.json", 'w') as f:
-                json.dump(json_data, f, indent=4)
-
 """
 {
     name: "Player Name",
@@ -92,3 +16,106 @@ if __name__ == "__main__":
     ]
 }
 """
+
+
+import json
+from nba_api.stats.endpoints import ShotChartDetail
+from nba_api.stats.static import players
+
+
+def getPlayerNamesFromFile(file_path: str) -> list[str]:
+    """
+    Read player names from a file and return them as a list.
+
+    Args:
+        file_path (str): The path to the file containing player names.
+
+    Returns:
+        list[str]: A list of player names.
+
+    """
+
+    with open(file_path, 'r') as file:
+        player_names = [line.strip() for line in file if line.strip()]
+    return player_names
+
+
+def getPlayerID(player_name: str) -> int or None:
+    """
+    Get the player ID for a given player name.
+
+    Args:
+        player_name (str): The name of the player.
+
+    Returns:
+        int: The player ID.
+    """
+    matches = players.find_players_by_full_name(player_name)
+    if matches:
+        return matches[0]['id']
+    return None
+
+
+def printPlayerIDs(player_names: list[str]) -> None:
+    """
+    Print player IDs for a list of player names.
+
+    Args:
+        player_names (list[str]): A list of player names.
+    """
+    for name in player_names:
+        player_id = getPlayerID(name)
+        if player_id:
+            print(f"\"{name}\": \"{player_id}\"")
+        else:
+            print(f"{name}: Not found")
+
+
+def getSeasonShots(player_id: int, season: str = '2024-25') -> list[dict]:
+    """
+    Gets the shot data for a specific player in a given season.
+
+    Args:
+        player_id (int): The ID of the player.
+        season (str): The season for which to retrieve shot data (default is '2024-25').
+
+    Returns:
+        list[dict]: A list of dictionaries containing shot data for the player.
+    """
+    shotlog = ShotChartDetail(
+        team_id = 0,  # 0 = all teams
+        player_id = player_id,
+        season_nullable = season,
+        season_type_all_star = 'Regular Season',
+        context_measure_simple = 'FGA'  # important to include missed shots
+    )
+
+    return shotlog.get_data_frames()[0]
+
+
+def main():
+    """
+    Main function to write player date to their own JSON files.
+    """
+    for name in getPlayerNamesFromFile('data/players.txt'):
+        players_id = getPlayerID(name)
+        if players_id:
+            shots = getSeasonShots(players_id, '2024-25')[["SHOT_MADE_FLAG", "LOC_X", "LOC_Y"]]
+
+            # Filter to only less than half court, then reorient to the center of half-court
+            shots = shots[shots['LOC_Y'].between(0, 564)].copy()
+            shots['LOC_Y'] = shots['LOC_Y'] - 282
+
+            json_data = {
+                "name": name,
+                "shots": shots.to_dict(orient='records')
+            }
+
+            # Saves as player_id.json to prevent accent file naming issues
+            with open(f"data/{players_id}.json", 'w') as f:
+                json.dump(json_data, f, indent=4)
+
+
+if __name__ == "__main__":
+    printPlayerIDs(getPlayerNamesFromFile('data/players.txt'))
+    main()
